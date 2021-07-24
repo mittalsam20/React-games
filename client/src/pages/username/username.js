6f0d5db9d093859e909f3af21b3cec932bd62ae6
SAMAKSH MITTAL <sam.mittal44@gmail.com>
2021-07-24 19:54:55 +0530

    highscore table created

--- client/src/pages/username/username.js
+++ client/src/pages/username/username.js
@@ -1,10 +1,33 @@
+import { Input } from "@material-ui/core";
+import { Button } from "@material-ui/core";
+import Scoretable from "../../components/scoretable/scoretable";
+import "./username.css";
 const Username = () => {
   return (
-    <div>
-      <form>
-        <input type="text" />
-        <button type="submit">GO..!!</button>
-      </form>
+    <div className="cont">
+      <div className="username-form">
+        <form>
+          <Input type="text" />
+          <Button
+            type="submit"
+            onClick={(e) => {
+              e.preventDefault();
+            }}
+          >
+            GO..!!
+          </Button>
+        </form>
+      </div>
+
+      <div className="highscoretable">
+        <div className="scoreheading">
+          <h1>LEADERBOARD</h1>
+        </div>
+        <div className="subheading">
+          <h3>Gotta Beat Them All..!!!</h3>
+        </div>
+        <Scoretable />
+      </div>
     </div>
   );
 };
