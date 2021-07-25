c97391384eadf1f01f4bcafef0ae64230d652bf6
SAMAKSH MITTAL <sam.mittal44@gmail.com>
2021-07-25 12:50:53 +0530

    logic for random hole and time written plus bug solved for the same

--- client/src/pages/molestart/molestart.js
+++ client/src/pages/molestart/molestart.js
@@ -5,6 +5,7 @@ import "./molestart.css";
 const Molestart = () => {
   const dirt = useRef([]);
   const [moledifficulty, setMoledifficulty] = useState("easy");
+  const [lasthole, setLasthole] = useState(null);
   const noOfDirts =
     moledifficulty === "easy"
       ? [1, 2, 3, 4, 5, 6, 7, 8, 9]
@@ -16,21 +17,40 @@ const Molestart = () => {
 
   const randomhole = (holes) => {
     const idx = Math.floor(Math.random() * holes.length);
-    console.log(idx);
-    if (idx === 5 || idx === 10 || idx === 0) return randomhole(dirt.current);
+    // console.log(idx);
     const hole = dirt.current[idx];
-    console.log(hole);
+    // console.log("last hole and this hole", lasthole, hole);
+    if (lasthole === hole) {
+      console.log("same same");
+      return randomhole(dirt.current);
+    }
+
+    if (moledifficulty === "easy") {
+      if (idx === 0 || idx === 5) {
+        return randomhole(dirt.current);
+      } else if (idx > 9) {
+        return randomhole(dirt.current);
+      }
+    } else {
+      if (idx === 0 || idx === 5 || idx === 10) {
+        return randomhole(dirt.current);
+      }
+    }
+    setLasthole(dirt.current[idx]);
+    // console.log(lasthole);
+    return hole;
   };
 
-  const dirtSelector = () => {
-    // console.log(dirt.current[2]);
+  const peep = () => {
+    const time = randomtime(200, 1000);
+    const hole = randomhole(dirt.current);
+    console.log(time, hole);
   };
 
   useEffect(() => {
-    // dirtSelector();
-    // console.log(dirt.current.length);
-    randomhole(dirt.current);
+    peep();
   }, [moledifficulty]);
+
   return (
     <div className="mole-cont">
       <div className="mole-left">
